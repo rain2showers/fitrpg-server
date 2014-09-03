8a760c71210a5948fee569f671948fdcadb5e04c
rain2showers <rain2showers@gmail.com>
2014-09-03 11:16:59 -0400

    updated the callback url.

--- server/fitbit/fitbit_controllers.js
+++ server/fitbit/fitbit_controllers.js
@@ -26,7 +26,7 @@ module.exports = exports = {
   fitbitStrategy: new FitbitStrategy({
     consumerKey: FITBIT_CONSUMER_KEY,
     consumerSecret: FITBIT_CONSUMER_SECRET,
-    callbackURL: 'http://testrpg.azurewebsites.net/fitbit/authcallback'
+    callbackURL: 'http://testrpg.azurewebsites.net/fitbit/auth'
     },
     function (token, tokenSecret, profile, done) {
       var timestamp = new Date();
